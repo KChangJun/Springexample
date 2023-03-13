966a7a9774da7f58a2238f7150c61f4072f3a280
ChangJun <projectchangjun@naver.com>
2023-03-13 21:00:23 +0900

    2023-03-13 Spring

---
+++ ex00/src/main/java/com/example/ex00/dependency/Coding.java
@@ -0,0 +1,14 @@
+package com.example.ex00.dependency;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component
+@Data
+public class Coding {
+@Autowired
+	private Computer computer;
+
+}
